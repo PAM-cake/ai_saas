8eda3c69e3cd94c1b5698e1d36b781f391e2f87f
Pam! <paammcakee@gmail.com>
2025-05-24 15:17:47 -0700

    Implement Clerk AUTH

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import Navitems from "./Navitems";
 
 const Navbar = () => {
@@ -8,17 +9,28 @@ const Navbar = () => {
       <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
           <Image 
-                src="/images/logo.svg"
-                alt="logo"
-                width={46} height={44} 
-            />
+            src="/images/logo.svg"
+            alt="logo"
+            width={46} 
+            height={44} 
+          />
         </div>
-    </Link>
-        <div className="flex items-center gap-8">
-            <Navitems />
-            <p>Sign In</p>
-        </div>
-      
+      </Link>
+      <div className="flex items-center gap-8">
+        <Navitems />
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+          <div className="flex gap-4">
+            <SignInButton mode="modal">
+              <button className="text-sm font-medium">
+                Sign In
+              </button>
+            </SignInButton>
+          </div>
+        </SignedOut>
+      </div>
     </nav>
   );
 };
