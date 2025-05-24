1b7bd15762338bddbcc1f5a3a663106cfbc2cb68
Pam! <paammcakee@gmail.com>
2025-05-24 01:41:13 -0700

    implement CompanionList_homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,51 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>welcome to my saas</h1>
-      <Button>
-        Get started
-      </Button>
-    </div>
+    <main className="px-4 md:px-16 py-8">
+      <h1 className="text-4xl font-bold mb-8">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard 
+          id="123"
+          name="Companion 1"
+          topic="Math"
+          subject="Algebra"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard 
+          id="456"
+          name="Companion 2"
+          topic="ENG"
+          subject="Literature"
+          duration={30}
+          color="#e5d0ff"
+
+        />
+        <CompanionCard 
+          id="789"
+          name="Companion 3"
+          topic="Sci"
+          subject="Algebra"
+          duration={30}
+          color="#BDE7FF"
+        />
+
+      </section>
+      <section className="home-section mt-8">
+        <CompanionList 
+            title ="Recent Completed Sessions"
+            companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
---
+++ components/CompanionCard.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ComapanionCardProps {
+    id: string;
+    name: string;
+    topic: string;
+    subject: string;
+    duration: number;
+    color: string;
+}
+
+const CompanionCard = ({id, name, topic, subject, duration, color}:
+    ComapanionCardProps) => {
+  return (
+    <article className="companion-card lg:max-w-[410px]" style={{backgroundColor: color}}>
+        <div className="flex justify-between items-center">
+            <div className="subject-badge">
+                {subject}
+            </div>
+            <button className="companion-bookmark">
+                <Image src="icons/bookmark.svg" alt="Bookmark" width={12.5} height={15} />
+            </button>
+        </div>
+        <h2 className="text-2xl font-bold mb-2">{name}</h2>
+        <p className="text-sm">{topic}</p>
+        <div className="flex items-center gap-2 mt-4">
+            <Image src="icons/clock.svg" alt="Clock" width={13.5} height={13.5} />
+            <p className="text-sm">
+              {duration} minutes
+            </p>
+        </div>
+        <Link href={`/companions/${id}`} className="w-full">
+            <button className="btn-primary w-full justify-center">
+                Start Learning
+            </button>
+        </Link>
+    </article>
+  )
+}
+
+export default CompanionCard
